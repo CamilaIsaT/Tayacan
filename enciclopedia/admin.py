fa6893dda4d6a49e362aa1c18c236708c31e449c
CamilaT <camilatrejosmartinez@gmail.com>
2025-09-29 08:19:05 -0600

    cambios en admin y creacion de superuser

--- enciclopedia/admin.py
+++ enciclopedia/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import rol, usuario, departamento, categoria, publicacion, comentario, imagen
+
+# Registra todos los modelos que deseas gestionar desde el admin
+admin.site.register(rol)
+admin.site.register(usuario)
+admin.site.register(departamento)
+admin.site.register(categoria)
+admin.site.register(publicacion)
+admin.site.register(comentario)
+admin.site.register(imagen)
